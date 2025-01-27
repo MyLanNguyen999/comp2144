1e7ef727a91b36ce94390384ecf3bbbedd70f275
MyLanNguyen999 <mylannguyen192@gmail.com>
2025-01-27 10:55:30 -0500

    finish the house and create 2 more instances

--- lesson-4/js/village.js
+++ lesson-4/js/village.js
@@ -14,54 +14,75 @@ const createScene = async function() {
     // Include a light
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
     
-    // STEP 1: Add a ground
+    //* STEP 1: Add a ground
     const ground = BABYLON.MeshBuilder.CreateGround("ground", {
         width: 10,
         height: 10
     });
-    // STEP 9: Colour the ground
+
+    //* STEP 9: Colour the ground
     const groundMat = new BABYLON.StandardMaterial("groundMat");
     groundMat.diffuseColor = new BABYLON.Color3(0.33, 0.42, 0.18);
     ground.material = groundMat;
 
-    // STEP 12b: Add an array to position the image properly on each of the four visible sides (notice we will not set 4 and 5)
-    //options parameter to set different images on each side
-       
+    //* STEP 12b: Add an array to position the image properly on each of the four visible sides (notice we will not set 4 and 5)
+    //@ options parameter to set different images on each side
+    const faceUV = [];
+    faceUV[0] = new BABYLON.Vector4(0.4, 0.0, 0.6, 1.0); //rear face
 
-    // STEP 2: Add a box to serve as a house
-    const box = BABYLON.MeshBuilder.CreateBox("box", {});
-    // STEP 12c: Change the above declaration to include a material wrap
-    
+    //@ Vector points are (x,y for bottom left and x,y for top right)
+    faceUV[1] = new BABYLON.Vector4(0.3, 0.0, 0.5, 1.0); //front face
 
-    // STEP 3a: Preview the result - noticing that the box is sunk into the ground
-    // STEP 3b: Adjust the vertical position of the box (default box height is 1 size unit)
+    faceUV[2] = new BABYLON.Vector4(0.6, 0.0, 1.0, 1.0); //right side
+    faceUV[3] = new BABYLON.Vector4(0.0, 0.0, 0.4, 1.0); //left side
+
+    //* STEP 2: Add a box to serve as a house
+    // const box = BABYLON.MeshBuilder.CreateBox("box", {});
+
+    //* STEP 12c: Change the above declaration to include a material wrap
+    const box = BABYLON.MeshBuilder.CreateBox("box", {faceUV: faceUV, wrap: true});
+
+    //* STEP 3a: Preview the result - noticing that the box is sunk into the ground
+    //* STEP 3b: Adjust the vertical position of the box (default box height is 1 size unit)
     box.position.y = 0.5;
-    // STEP 5a: Scale the box to resemble more of a house shape
+
+    //* STEP 5a: Scale the box to resemble more of a house shape
     box.scaling.x = 2;
     box.scaling.y = 1.5;
     box.scaling.z = 3;
-    // STEP 5b: ...or use a vector object to scale the box instead
+
+    //* STEP 5b: ...or use a vector object to scale the box instead
     // box.position = new BABYLON.Vector3(1, 0.75, 5)
-    // STEP 6a: Repostion the box (which is now 1.5 units in height, so to sit on the ground, we need to raise y to 0.75)
+
+    //* STEP 6a: Repostion the box (which is now 1.5 units in height, so to sit on the ground, we need to raise y to 0.75)
     box.position.x = 1;
     box.position.y = 0.75;
     box.position.z = 2;
-    // STEP 6b: ...or with a vector object
+
+    //* STEP 6b: ...or with a vector object
     
-    // STEP 7: Rotate the box (Babylon.JS uses radians - so convert if you wish)
+    //* STEP 7: Rotate the box (Babylon.JS uses radians - so convert if you wish)
     // box.rotation.y = Math.PI / 4;
     box.rotation.y = BABYLON.Tools.ToRadians(45);
-    // STEP 11: Add a texture to the walls of the house (the box) (https://www.babylonjs-playground.com/textures/floor.png)
-        
-    // STEP 12a: Change the texture above to use an image with doors and windows instead
+
+    //* STEP 11: Add a texture to the walls of the house (the box) (https://www.babylonjs-playground.com/textures/floor.png)
+    const boxMat = new BABYLON.StandardMaterial("boxMat");
+    // boxMat.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png");
+    boxMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/textures/semihouse.png");
+    box.material = boxMat;
     
-    // STEP 8a: Build a roof - using a cylinder mesh
+        
+    //* STEP 12a: Change the texture above to use an image with doors and windows instead
+    
+
+    //* STEP 8a: Build a roof - using a cylinder mesh
     const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {
         diameter: 2.8,
         height: 3.5,
         tessellation: 3
     });
-    // STEP 8b: Scale, rotate, and position the new mesh object
+
+    //* STEP 8b: Scale, rotate, and position the new mesh object
     roof.scaling.x = 0.75;
     // roof.rotation.z = Math.PI / 2;
 
@@ -72,23 +93,39 @@ const createScene = async function() {
     roof.position.x = 1;
     roof.position.z = 2;
 
-    //* START HERE ON JANUARY 27, 2025
+    //! START HERE ON JANUARY 27, 2025
     
-    // STEP 10: Add a texture to the roof (https://assets.babylonjs.com/environments/roof.jpg)
+    //* STEP 10: Add a texture to the roof (https://assets.babylonjs.com/environments/roof.jpg)
     const roofMat = new BABYLON.StandardMaterial("roofMat");
     roofMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/roof.jpg", scene);
     roof.material = roofMat;
 
-    // STEP 13a: Let's combine the box and the roof meshes into one mesh called 'house'
-    
-    // STEP 13b: Yikes - now the two meshes share the same material - we must allow multiple materials within the same mesh
+    //* STEP 13a: Let's combine the box and the roof meshes into one mesh called 'house'
+    // const house = BABYLON.Mesh.MergeMeshes([box, roof]); //@ Merge the box and roof meshes into one mesh called 'house', this way does not work because it merged the walls into the roof so the whole house covered with windows and doors
+
+    const house = BABYLON.Mesh.MergeMeshes([box, roof], true, false, null, false, true); //@ Merge the box and roof meshes into one mesh called 'house'
+
+    //* STEP 13b: Yikes - now the two meshes share the same material - we must allow multiple materials within the same mesh
     
 
-    // STEP 14a: Create another instance of the house object and place it elsewhere on the ground
-    
-    // STEP 14b: How about a third house?
+    //* STEP 14a: Create another instance of the house object and place it elsewhere on the ground
+    let house2 = house.createInstance("house2");
+    //@ postion the house - option 1
+    // house2.position.x = 0;
+    // house2.position.y = 0;
+    // house2.position.z = -4;
 
-    // STEP 4: Add some ambient sounds ("Chirping Birds Ambience" by Alex from Pixabay - https://pixabay.com/sound-effects/search/birds%20chirping/)
+    //@ postion the house - option 2
+    house2.position = new BABYLON.Vector3(0, 0, -4);
+
+    house2.rotation.y = BABYLON.Tools.ToRadians(45);
+    
+    //* STEP 14b: How about a third house?
+    let house3 = house.createInstance("house3");
+    house3.position = new BABYLON.Vector3(-1, 0, 1);
+    house3.rotation.y = BABYLON.Tools.ToRadians(-45);
+
+    //* STEP 4: Add some ambient sounds ("Chirping Birds Ambience" by Alex from Pixabay - https://pixabay.com/sound-effects/search/birds%20chirping/)
     const sound = new BABYLON.Sound("birds", "./media/chirping-birds-ambience-217410.mp3", scene, null, {
         loop: true,
         autoplay: true
